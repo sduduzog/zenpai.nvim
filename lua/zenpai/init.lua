da58a5a73d2337f10b1243e83c3ff366e0dad446
vt <me@vtcodes.dev>
2024-11-06 20:25:30 +0200

    add git commands to stage files & generate a diff

--- lua/zenpai/init.lua
+++ lua/zenpai/init.lua
@@ -1,9 +1,40 @@
 local M = {}
 
 local window = require "zenpai.window"
+local git_cmd = require "zenpai.git_cmd"
 
 local function on_prompt_input(input)
-  local response = "Response to: " .. input
+  local response = ""
+
+  if input ~= "gen" then
+    return
+  end
+
+  if not git_cmd.is_git_repo() then
+    response = "not a git repository."
+  elseif not git_cmd.has_changes() then
+    response = "no changes to commit."
+  else
+    local files_to_stage = git_cmd.files_to_be_staged()
+    if not files_to_stage then
+      return
+    end
+
+    local prompt_msg = string.format("%s\nstage these files? (y/n): ", files_to_stage)
+    local confirm = vim.fn.input(prompt_msg):lower()
+
+    if confirm ~= "y" and confirm ~= "yes" then
+      vim.notify("staging aborted.", vim.log.levels.INFO)
+      return
+    end
+
+    git_cmd.stage_files()
+    vim.notify("files staged successfully.", vim.log.levels.INFO)
+
+    local diff = git_cmd.get_diff()
+    vim.notify(diff, vim.log.levels.INFO)
+  end
+
   local buf = window.get_state().buf
   if buf and vim.api.nvim_buf_is_valid(buf) then
     vim.api.nvim_buf_set_lines(buf, -1, -1, false, { response })
@@ -21,7 +52,6 @@ end
 
 function M.setup(opts)
   opts = opts or {}
-
   vim.keymap.set("n", "<Leader>i", toggle_window)
 end
 
